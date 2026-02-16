37a77c09420741707e5d4f7086ff3948077cb034
Вадим Максимов <dignot96@gmail.com>
2026-02-16 20:40:44 +0300

    fix: update eslint and related dependencies

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -1,14 +1,14 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,27 +20,11 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
-      "prefer-const": [
-        "error",
+      'prefer-const': [
+        'error',
         {
-          destructuring: "any",
+          destructuring: 'any',
           ignoreReadBeforeAssign: false,
-        },
-      ],
-      "import/order": [
-        "error",
-        {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
-            "type",
-          ],
-          "newlines-between": "always",
-          alphabetize: { order: "asc", caseInsensitive: true },
         },
       ],
     },
